966f7a5b87e37af8a9f07f444115df406c430fea
MDKalenderoglu <mdkalenderoglu@gmail.com>
2026-07-25 01:31:43 +0300

    feat: configure GitHub Pages base URL

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  site: 'https://mdkalenderoglu.github.io',
+  base: '/GeneSite',
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
